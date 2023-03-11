782ed96d88af6ff3b872b6b0930193f0f207dbbe
GongLiTian <gongc9@qq.com>
2023-03-11 21:27:10 +0800

    Changes to be committed: 	modified:   .gitignore 	modified:   Mainwindow.py 	modified:   Mainwindow.ui 	modified:   README.md 	modified:   main.py 	new file:   utils.py

--- README.md
+++ README.md
@@ -4,18 +4,36 @@
 - [x] 核心功能
 - [x] 流式传输
 - [x] 代码块渲染
-- [ ] 异常捕获
+- [x] 异常捕获
 - [ ] 快捷键绑定
 - [ ] 按钮功能完善
   - [x] 发送:不能发送空字符串
   - [x] 停止:强制停止生成回答
   - [ ] 保存对话
   - [ ] 重新生成对话
-  - [ ] 删除上条对话、
+  - [x] 删除上条对话
+  - [ ] 对summary进程优化
   - [ ] 总结对话
+  - [ ] 自动总结对话标题并保存
   - [ ] 新的对话
 - [ ] 按钮样式（图标）
 - [ ] 显示tokens
 - [ ] 计算速度
 - [ ] 收集模版
+
+## 打包
+
+### 1 zipimporter has no attribute exec_module
+
+```python
+from zipimport import zipimporter
+
+def create_module(self, spec):
+    return None
+zipimporter.create_module = create_module
+
+def exec_module(self, module):
+    exec(self.get_code(module.__name__), module.__dict__)
+zipimporter.exec_module = exec_module
+```
 
--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication
-from PyQt5.QtCore import QThread, pyqtSignal  # 导入线程模块
+import os
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+import openai
 from PyQt5 import QtGui
-from PyQt5.QtGui import QTextCursor
 from Mainwindow import Ui_MainWindow
 import markdown
 import requests, json
-import traceback
+from utils import *
 
+apiKey = "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"
+openai.api_key = apiKey
 
 API_URL = "https://api.openai.com/v1/chat/completions"
 headers = {"Content-Type": "application/json", "Authorization": f"Bearer {apiKey}"}
 
+global bot, bot_format
 bot = []
 bot_format = []
 botFlag = True
@@ -44,23 +50,29 @@ class DemoMain(QMainWindow, Ui_MainWindow):
         # 初始化界面
         self.setupUi(self)
         self.initQTextBrowser()
+        self.loadHistory()
         # 绑定逻辑
         self.send_btn.setEnabled(False)
         self.send_btn.clicked.connect(self.startRequestTask)
         self.shut_btn.clicked.connect(self.shutRequestTask)
         self.user_text.textChanged.connect(self.dealNoInput)
+        self.saveChat_btn.clicked.connect(self.saveHistory)
+        self.summary_btn.clicked.connect(self.Summary)
+        self.deleteLast_btn.clicked.connect(self.deleteLastChat)
         # self.test_btn.clicked.connect(self.addBotFrame)
 
         #
         self.globalHtml = []
         self.threadList = []
+        self.loadedHistory = []
+        self.itemList = []
         #
         self.unrenderStr = ""
         self.curRenderLength = 0
         self.rerenderCount = 0
 
     def startRequestTask(self):
-        self.updataStatus("等待回答")
+        self.updateStatus("等待回答")
         self.send_btn.setEnabled(False)
         self.shut_btn.setEnabled(True)
         requestTask = RequestTask(self.user_text.toPlainText())
@@ -69,7 +81,7 @@ class DemoMain(QMainWindow, Ui_MainWindow):
 
         requestTask.renderSignal.connect(self.render)
         requestTask.finishSignal.connect(self.finishRequestTask)
-        requestTask.stateUpdateSignal.connect(self.updataStatus)
+        requestTask.stateUpdateSignal.connect(self.updateStatus)
 
         requestTask.start()
 
@@ -77,23 +89,26 @@ class DemoMain(QMainWindow, Ui_MainWindow):
         if self.user_text.toPlainText():
             self.send_btn.setEnabled(True)
         self.shut_btn.setEnabled(False)
+        self.threadList = []
+
         # print(self.bot_text.toHtml())
 
     def shutRequestTask(self):
-        self.updataStatus("中止生成回答")
+        self.updateStatus("中止生成回答")
         if len(bot) != 0:
             bot.pop()
             bot_format.pop()
         for thread in self.threadList:
             thread.finishSignal.emit()
             thread.runPermission = False
+            del thread
         self.shut_btn.setEnabled(False)
 
     def render(self):
         self.bot_text.clear()
         html = ""
         for chat in bot:
-            # print(chat[1])
+            # print(chat)
             markedContent = markdown.markdown(
                 chat[1], extensions=["fenced_code", "codehilite"]
             )
@@ -104,7 +119,7 @@ class DemoMain(QMainWindow, Ui_MainWindow):
         self.bot_text.moveCursor(QtGui.QTextCursor.End)
         # python 线性回归
 
-    def updataStatus(self, message: str):
+    def updateStatus(self, message: str):
         self.statusBar().showMessage(message)
 
     def initQTextBrowser(self):
@@ -157,70 +172,159 @@ class DemoMain(QMainWindow, Ui_MainWindow):
         else:
             self.send_btn.setEnabled(False)
 
+    def saveHistory(self):
+        summary = self.summaryTitle()
+        bot.insert(0, ["title", summary])
+        m = len(os.listdir("./user"))
+        with open(f"./user/history.pickle_{m}", "wb") as f:
+            pickle.dump(bot, f)
+            m += 1
+
+    def loadHistory(self, filePath: str = "./user"):  # list[0] = ["user","hello!"]
+        # 从文件中读取并反序列化列表
+        for i in range(len(os.listdir(filePath))):
+            with open(f"{filePath}/historyList.pickle_{i}", "rb") as f:
+                history = pickle.load(f)
+                self.loadedHistory.append(history[1:])
+                self.itemList.append(history[0][1])
+                self.historyList.addItem(history[0][1])
+
+    def summaryTitle(self):
+        self.Summary()
+        while len(self.threadList) != 0:
+            print(len(self.threadList))
+        title = bot[-1][1]
+        bot.pop()
+        bot.pop()
+        bot_format.pop()
+        bot_format.pop()
+        self.render()
+        return title
+
+    def Summary(self):
+        input = "请帮我总结一下上述对话的内容，实现减少字数的同时，保证对话的质量。在总结中不要加入这一句话。"
+        self.updateStatus("等待回答")
+        self.send_btn.setEnabled(False)
+        self.shut_btn.setEnabled(True)
+        requestTask = RequestTask(input)
+        self.user_text.clear()
+        self.threadList.append(requestTask)
+
+        requestTask.renderSignal.connect(self.render)
+        requestTask.finishSignal.connect(self.finishRequestTask)
+        requestTask.stateUpdateSignal.connect(self.updateStatus)
+        requestTask.errorSignal.connect(self.showErrorMessage)
+        requestTask.start()
+
+    def deleteLastChat(self):
+        reply = QMessageBox.question(
+            self,
+            "Delete Item",
+            "Are you sure to delete last Chat?",
+            QMessageBox.Yes | QMessageBox.No,
+            QMessageBox.No,
+        )
+        if reply == QMessageBox.Yes:
+            bot.pop()
+            bot.pop()
+            bot_format.pop()
+            bot_format.pop()
+            print(bot)
+            self.render()
+
+    def showErrorMessage(error: str):
+        # 创建一个警告框
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Critical)
+
+        # 设置警告框的标题和正文
+        msg.setWindowTitle("Error")
+        msg.setText("Error detected in operation.")
+        msg.setInformativeText(error)
+        # 显示警告框
+        msg.exec()
+
 
 # mmmmmmmmmmnmjkhjbjnja
-# self.bot_text.setText(answer)
 
 
 class RequestTask(QThread):
     renderSignal = pyqtSignal()
     finishSignal = pyqtSignal()
     stateUpdateSignal = pyqtSignal(str)
+    errorSignal = pyqtSignal(str)
 
-    def __init__(self, inputSentence: str):
+    def __init__(self, inputSentence: str, summaryFlag: bool):
         super(RequestTask, self).__init__()
         self.inputSentence = inputSentence
         self.runPermission = True
+        self.summaryFlag = summaryFlag
 
     def run(self):
         bot.append([userFlag, self.inputSentence])
         bot_format.append({"role": "user", "content": self.inputSentence})
         self.renderSignal.emit()
-        payload = {
-            "model": "gpt-3.5-turbo",
-            "messages": bot_format,  # [{"role": "user", "content": f"{inputs}"}],
-            "temperature": 1,  # 1.0,
-            "top_p": 1,  # 1.0,
-            "n": 1,
-            "stream": True,
-            "presence_penalty": 0,
-            "frequency_penalty": 0,
-        }
-        print(bot_format)
-        partialWords = ""
-        try:
-            response = requests.post(
-                API_URL, headers=headers, json=payload, stream=True
-            )
-        except Exception as e:
-            traceback.print_exc()
-        bot.append([botFlag, partialWords])
-        bot_format.append({"role": "assistant", "content": partialWords})
-        for chunk in response.iter_lines():
-            self.stateUpdateSignal.emit("回答生成中")
-            # print(f"chunk:{chunk}")
-            if chunk:
+
+        if not self.summaryFlag:
+            payload = {
+                "model": "gpt-3.5-turbo",
+                "messages": bot_format,  # [{"role": "user", "content": f"{inputs}"}],
+                "temperature": 1,  # 1.0,
+                "top_p": 1,  # 1.0,
+                "n": 1,
+                "stream": True,
+                "presence_penalty": 0,
+                "frequency_penalty": 0,
+            }
+            print(bot_format)
+            with requests.Session() as session:
                 try:
-                    data = json.loads(chunk.decode()[6:])
-                    delta = data["choices"][0]["delta"]
-                    # print(data)
-                    if len(delta) == 0:
-                        break
+                    response = session.post(
+                        API_URL, headers=headers, json=payload, stream=True
+                    )
+                    partialWords = ""
+                    # 新增空元素，准备接受流式传输内容
+                    bot.append([botFlag, partialWords])
+                    bot_format.append({"role": "assistant", "content": partialWords})
+                    # 开始获取流式传输内容
+                    for chunk in response.iter_lines():
+                        self.stateUpdateSignal.emit("回答生成中")
+                        if not self.runPermission:
+                            response.raw.close()
+                            return self.finishSignal.emit()  # 中止函数并发出中止信号
+                        if chunk:
+                            try:
+                                data = json.loads(chunk.decode()[6:])
+                                delta = data["choices"][0]["delta"]
+                                # print(data)
+                                if len(delta) == 0:
+                                    break
+                            except Exception as e:
+                                self.errorSignal.emit(f"{e}")
+                                break
+                            status_text = f"id: {data['id']}, finish_reason: {data['choices'][0]['finish_reason']}"
+                            partial = delta["content"] if "content" in delta else ""
+                            bot[-1][1] += partial
+                            bot_format[-1]["content"] += partial
+                            self.renderSignal.emit()
                 except Exception as e:
-                    traceback.print_exc()
-                    break
-                status_text = f"id: {data['id']}, finish_reason: {data['choices'][0]['finish_reason']}"
-                partial = delta["content"] if "content" in delta else ""
-                if not self.runPermission:
-                    del response  # 传输流不会立即停止
-                    return self.finishSignal.emit()
-                bot[-1][1] += partial
-                bot_format[-1]["content"] += partial
-                self.renderSignal.emit()
-            if not self.runPermission:
-                del response  # 传输流不会立即停止
-                return self.finishSignal.emit()
-        self.stateUpdateSignal.emit("生成完毕")
+                    self.errorSignal.emit(f"{e}")
+
+            self.stateUpdateSignal.emit("生成完毕")
+        else:
+            try:
+                response = openai.ChatCompletion.create(
+                    model="gpt-3.5-turbo",
+                    messages=bot_format,
+                )
+                content = response["choices"][0]["message"]["content"]
+                totalTokens = response["usage"]["total_tokens"]
+                bot.append(content)
+                bot_format.append({"role": "assistant", "content": content})
+                self.stateUpdateSignal.emit("生成完毕")
+            except Exception as e:
+                self.errorSignal.emit(f"{e}")
+
         self.finishSignal.emit()
 
 
